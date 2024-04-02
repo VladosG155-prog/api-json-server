83e4b3c5c06a0fe240ddeebc0de4bc196c495a1f
Владислав Колос <vladislavkolos@Vladislavs-MacBook-Air.local>
2024-04-02 13:48:30 +0300

    update pagination

--- server.js
+++ server.js
@@ -11,33 +11,7 @@ server.use(jsonServer.bodyParser);
 
 // Обновляем маршрут для обработки пагинации
 server.use((req, res, next) => {
-  if (!req.query._page || !req.query._limit) {
-    next();
-    return;
-  }
-  const page = parseInt(req.query._page) || 1;
-  const limit = parseInt(req.query._limit) || 10;
-  const start = (page - 1) * limit;
-  const path = req.path.split("/").filter(Boolean).join(".");
-  const results = router.db.get(path).value();
-  const paginatedResults = results.slice(start, start + limit);
-  const totalResults = results.length;
-  const totalPages = Math.ceil(totalResults / limit);
-  const hasNext = page < totalPages;
-  const hasPrev = page > 1;
-
   res.setHeader("X-Total-Count", totalResults);
-  res.send({
-    results: paginatedResults,
-    hasNext,
-    hasPrev,
-  });
-  res.locals.data = {
-    results: paginatedResults,
-    hasNext,
-    hasPrev,
-  };
-  next();
 });
 server.use(router);
 
